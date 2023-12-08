81f7f5ef812dd46f102d52b40f621649b5dd0037
mchughs <smchugh230395@gmail.com>
2023-12-08 10:11:02 +0100

    day 8 solution part 2

--- src/day08/solution.clj
+++ src/day08/solution.clj
@@ -1,5 +1,6 @@
 (ns day08.solution
-  (:require [clojure.string :as string]))
+  (:require [clojure.set :as set]
+            [clojure.string :as string]))
 
 ;; solution part 1
 
@@ -23,4 +24,34 @@
        (recur remaining (inc steps) (get-in nodes [loc instruction]))))))
 
 ;; solution part 2
+
+(defn lcm [xs]
+  (letfn [(gcd [a b]
+            (if (zero? b)
+              a
+              (recur b (mod a b))))]
+    (reduce (fn [a b]
+              (quot (* a b) (gcd a b)))
+            1
+            xs)))
+
+(time
+ (let [[lr _ & remaining] (->> (slurp "src/day08/input.txt")
+                               string/split-lines)
+       nodes (reduce (fn [acc s]
+                       (let [[node left right] (re-seq #"[A-Z]{3}" s)]
+                         (assoc acc node {\L left \R right})))
+                     {}
+                     remaining)
+       instructions (cycle lr)
+       starting-points (filter #(= \A (last %)) (keys nodes))
+       end-points (set (filter #(= \Z (last %)) (keys nodes)))]
+   (->> starting-points
+        (map #(loop [[instruction & remaining] instructions
+                     steps 0
+                     loc %]
+                (if (contains? end-points loc)
+                  steps
+                  (recur remaining (inc steps) (get-in nodes [loc instruction])))))
+        lcm)))
 
