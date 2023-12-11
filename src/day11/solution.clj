72b8875f0380dc40264e7c6d3ef3658a1ab9f0f6
mchughs <smchugh230395@gmail.com>
2023-12-11 12:07:51 +0100

    day 11 solution part 2

--- src/day11/solution.clj
+++ src/day11/solution.clj
@@ -48,4 +48,42 @@
 
 ;; solution part 2
 
- 
+(defn expand-row* [input]
+  (->> input
+       (map-indexed vector)
+       (filter (fn [[_ row]]
+                 (every? #(= \. %) row)))
+       (map first)))
+
+(defn- expand-space* [input]
+  (let [ys (expand-row* input)
+        xs (expand-row* (transpose input))]
+    [xs ys]))
+
+(defn adjust "Puts the x,y point at its real x,y position in the expanded universe"
+  [[xs ys] [x y]]
+  (let [expansion-factor 1000000
+        dx (* (dec expansion-factor) (count (first (split-with (partial > x) xs))))
+        dy (* (dec expansion-factor) (count (first (split-with (partial > y) ys))))]
+    [(+ x dx) (+ y dy)]))
+
+(time
+ (let [input (string/split-lines (slurp "src/day11/input.txt"))
+       [xs ys] (expand-space* input)
+       galaxies (->> input
+                     (map-indexed vector)
+                     (reduce (fn [acc [y line]]
+                               (into acc
+                                     (comp
+                                      (map-indexed vector)
+                                      (filter (fn [[_ tile]] (= \# tile)))
+                                      (map first)
+                                      (map (fn [x] [x y]))
+                                      (map (partial adjust [xs ys])))
+                                     line))
+                             #{}))]
+   (transduce
+    (map manhattan-distance)
+    +
+    0
+    (combo/combinations galaxies 2))))
