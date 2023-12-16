f08938b02454f33715e7924e849ca2e55cb1a716
mchughs <smchugh230395@gmail.com>
2023-12-16 13:46:44 +0100

    day 16 solution part 2

--- src/day16/solution.clj
+++ src/day16/solution.clj
@@ -1,5 +1,6 @@
 (ns day16.solution
-  (:require [clojure.string :as string]))
+  (:require [clojure.set :as set]
+            [clojure.string :as string]))
 
 ;; solution part 1
 
@@ -28,47 +29,101 @@
    :east (fn [[x y]] [(inc x) y])
    :west (fn [[x y]] [(dec x) y])})
 
-(defn step [chart {position :position facing :facing}]
-  (let [[x y :as tile-position] ((get next-tile facing) position)
-        tile-type (get-in chart [y x])
-        facing-fn (get tile-types tile-type)]
-    (if (nil? tile-type)
-      []
-      (mapv (fn [a b]
-              {:facing a
-               :position b})
-            (facing-fn facing)
-            (repeat tile-position)))))
+(defn step-fn [chart [max-x max-y]]
+  (fn [{position :position facing :facing}]
+    (let [[x y :as tile-position] ((get next-tile facing) position)
+          tile-type (get-in chart [y x])
+          facing-fn (get tile-types tile-type)]
+      (if (or (nil? tile-type)
+              (not (< -1 x max-x))
+              (not (< -1 y max-y)))
+        '()
+        (map (fn [a b]
+               {:facing a
+                :position b})
+             (facing-fn facing)
+             (repeat tile-position))))))
 
-(defn step-all-fn [input [max-x max-y]]
+(defn step-all-fn [step]
   (fn [{:keys [beam-heads visited]}]
-    (let [new-beam-heads (->> beam-heads
-                              (mapcat #(step input %))
-                              (filter (fn [{[x y] :position}]
-                                        (and (< -1 x max-x)
-                                             (< -1 y max-y)))))
-          new-visited (into visited
-                            (map :position)
-                            new-beam-heads)]
+    ;; takes previous travesed paths out.
+    (let [new-beam-heads (set/difference (into #{} (mapcat step) beam-heads)
+                                         visited)
+          new-visited (into visited new-beam-heads)]
       {:beam-heads new-beam-heads
        :visited new-visited})))
 
+;; ~300 ms
 (time
  (let [input (string/split-lines (slurp "src/day16/input.txt"))
        [max-x max-y] ((juxt (comp count first) count) input)
-       step-all (step-all-fn input [max-x max-y])]
+       step (memoize (step-fn input [max-x max-y]))
+       step-all (memoize (step-all-fn step))]
    (->> {:beam-heads '({:position [-1 0]
                         :facing :east})
          :visited #{}}
         (iterate step-all)
-        (map (comp count :visited))
-        ;; 10 is a heuristic for the number of repetitions of visited amounts to expect.
-        ;; the beams can repeat the amount of places they visited a few time in a row.
-        ;; to improve this we should also track the facing direction on each visited point.
-        ;; then we could probably lower this down to 2 instead of 10.
-        (partition 10 1)
-        (take-while #(apply not= %))
+        (take-while (comp seq :beam-heads))
         last
-        last)))
+        step-all ;; take one last step
+        :visited
+        (into #{} (map :position))
+        count)))
 
 ;; solution part 2
+
+(defn get-corners [[max-x max-y]]
+  [{:position [-1 0]
+    :facing :east}
+   {:position [0 -1]
+    :facing :south}
+   {:position [max-x 0]
+    :facing :west}
+   {:position [(dec max-x) -1]
+    :facing :south}
+   {:position [-1 (dec max-y)]
+    :facing :east}
+   {:position [0 max-y]
+    :facing :north}
+   {:position [max-x (dec max-y)]
+    :facing :west}
+   {:position [(dec max-x) max-y]
+    :facing :north}])
+
+(defn get-edges [[max-x max-y]]
+  (concat
+   (for [x (range 0 max-x)]
+     {:position [x 0]
+      :facing :south})
+   (for [x (range 0 max-x)]
+     {:position [x (dec max-y)]
+      :facing :north})
+   (for [y (range 0 max-y)]
+     {:position [0 y]
+      :facing :east})
+   (for [y (range 0 max-y)]
+     {:position [(dec max-x) y]
+      :facing :west})))
+
+;; ~8500 ms
+(time
+ (let [input (string/split-lines (slurp "src/day16/input.txt"))
+       [max-x max-y] ((juxt (comp count first) count) input)
+       step (memoize (step-fn input [max-x max-y]))
+       step-all (step-all-fn step)
+       corners (get-corners [max-x max-y])
+       edges (get-edges [max-x max-y])
+       entry-points (concat corners edges)]
+   (apply max
+          (for [point entry-points]
+            (->> {:beam-heads #{point}
+                  :visited #{}}
+                 (iterate step-all)
+                 (take-while (comp seq :beam-heads))
+                 last
+                 step-all
+                 :visited
+                 (into #{} (map :position))
+                 count
+                 inc ;; Not sure exactly why but have to add one for my input. I guess the start point doesn't get counted as visited for some reason :shrug
+                 )))))
