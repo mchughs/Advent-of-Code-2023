bc2104349f8eafc168f3df8d559b165072a8a33e
mchughs <smchugh230395@gmail.com>
2023-12-19 14:49:20 +0100

    day 19 solution part 2

--- src/day19/solution.clj
+++ src/day19/solution.clj
@@ -44,3 +44,61 @@
         (apply +))))
 
  ;; solution part 2
+
+(defn parse-workflow* [s]
+  (let [[_ label exprs default] (re-matches #"(\w+)\{(.*?),([RA]|\w+)\}" s)
+        expressions (map rest (re-seq #"([xmas])([><])(\d+)\:([RA]|\w+)" exprs))
+        len (count expressions)]
+    (into {}
+          (map-indexed (fn [idx [variable op n target]]
+                         [(str label idx)
+                          [variable op n (or (#{"A" "R"} target)
+                                             (str target 0))
+                           (let [nxt-idx (inc idx)]
+                             (if (< nxt-idx len)
+                               (str label nxt-idx)
+                               (or (#{"A" "R"} default)
+                                   (str default 0))))]]))
+          expressions)))
+
+(defmacro build-splitter [variable op n]
+  `(fn [parts#]
+     (let [k# (keyword ~variable)
+           n# (Integer. ~n)]
+       (if (= "<" ~op)
+         [(assoc-in parts# [k# 1] (dec n#)) (assoc-in parts# [k# 0] n#)]
+         [(assoc-in parts# [k# 0] (inc n#)) (assoc-in parts# [k# 1] n#)]))))
+
+(defn build-tree [m k]
+  (if-let [[variable op n left right] (get m k)]
+    {:label k
+     :condition (build-splitter variable op n)
+     :children [(build-tree m left) (build-tree m right)]}
+    k))
+
+(defn count-combos
+  "(count-combos {:x [1 4000] :m [1 4000] :a [1 4000] :s [1 4000]})
+   => 256000000000000"
+  [parts]
+  (reduce-kv (fn [acc _ [a b]]
+               (* acc (inc (- b a))))
+             1
+             parts))
+
+(defn count-node [{:keys [condition children] :as node} parts]
+  (case node
+    "R" 0
+    "A" (count-combos parts)
+    (let [[l-child r-child] children
+          [l-parts r-parts] (condition parts)]
+      (+ (count-node l-child l-parts)
+         (count-node r-child r-parts)))))
+
+;; ~20ms
+(time
+ (let [[workflow-input _] (map string/split-lines (string/split (slurp "src/day19/input.txt") #"\n\n"))
+       workflow (into {} (map parse-workflow*) workflow-input)
+       root "in0"
+       init {:x [1 4000] :m [1 4000] :a [1 4000] :s [1 4000]}
+       tree (build-tree workflow root)]
+   (count-node tree init)))
